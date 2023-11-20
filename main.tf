055f28024d818a4276cec288dfbfc620a52c83bd
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-11-20 11:23:27 +0300

    Isolatiaon vai WOrkspace

--- main.tf
+++ main.tf
@@ -21,7 +21,12 @@ terraform {
 
 }
 
+provider "aws" {
+  region = "us-east-2"
+}
+
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
-    instance_type = "t2.micro"
+    instance_type = terraform.workspace == "default" ? "t2.medium": "t2.micro"
+
 }
