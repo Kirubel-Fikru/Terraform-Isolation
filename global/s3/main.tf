2253eb4630072879cdc8ef03693577d7851f4690
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-11-20 12:49:58 +0300

    Isolatiaon vai File Layout

---
+++ global/s3/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+    # Configure Terraform to store the state in your S3 bucket (with encryption and locking)
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-state-key20231117084134961500000001"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+ }
+
+}
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+# create an S3 bucket by using the aws_s3_bucket resource:
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "terraform-state-key20231117084134961500000001"
+    # Prevent accidental deletion of this S3 bucket
+    lifecycle {
+        prevent_destroy = true 
+    }
+}
+
+# Enable versioning so you can see the full revision history of your state files
+resource "aws_s3_bucket_versioning" "enabled" {
+    bucket = aws_s3_bucket.terraform_state.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+    bucket = aws_s3_bucket.terraform_state.id
+    rule {
+        apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+        }
+    }
+}
+
+# Explicitly block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+    bucket = aws_s3_bucket.terraform_state.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
+
+# create a DynamoDB table to use for locking
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-up-and-running-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
